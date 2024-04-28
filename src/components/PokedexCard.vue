8571512f541ff38a27a805d48fe52ede43f3e2b5
Alexander Jansen <a.jansen@reanmo.com>
2024-04-28 02:13:44 +0200

    Add PokeballIcon to PokedexCard

--- src/components/PokedexCard.vue
+++ src/components/PokedexCard.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="gap-default2 bg-grass flex h-28 flex-col rounded-2xl bg-opacity-80 p-3.5 text-white">
+  <div
+    class="gap-default2 bg-grass relative z-10 flex h-28 flex-col rounded-2xl bg-opacity-80 p-3.5 text-white"
+  >
     <span class="text-sm font-bold">Bulbasauer</span>
     <TypeChip type="Grass" />
     <TypeChip type="Poison" />
+    <PokeballIcon
+      class="absolute -bottom-3.5 -right-4 h-24 opacity-30"
+      color-code1="#FFFFFF"
+      color-code2="#FFFFFF"
+      color-code3="#FFFFFF"
+      color-code4="#FFFFFF"
+      color-code5="#FFFFFF00"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import TypeChip from '@/components/TypeChip.vue'
+import PokeballIcon from '@/components/icons/PokeballIcon.vue'
 </script>
