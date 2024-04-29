9a4c065f6e49c937652554ccf1185e9c99796c8c
Alexander Jansen <a.jansen@reanmo.com>
2024-04-29 10:50:14 +0200

    Add firebase hosting and GitHub actions deploy

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { initializeApp } from 'firebase/app'
 
 import App from './App.vue'
 import router from './router'
@@ -12,3 +13,14 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyANEIuuh6JavQERtVLHzS1FeqdRoAEtsVM',
+  authDomain: 'vue-pokedex-3330a.firebaseapp.com',
+  projectId: 'vue-pokedex-3330a',
+  storageBucket: 'vue-pokedex-3330a.appspot.com',
+  messagingSenderId: '328460233350',
+  appId: '1:328460233350:web:fd7af89465c9b6f52eff07'
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
