fc0548a11dcc5d62251ca7b2b18192a6bdd102b4
Alexander Jansen <a.jansen@reanmo.com>
2024-05-05 13:19:01 +0200

    Fetch pokemon details

--- src/views/PokemonDetailView.vue
+++ src/views/PokemonDetailView.vue
@@ -1,5 +1,20 @@
 <template>
-  <main class="px-default">Pokemon Detail View</main>
+  <main class="px-default">{{ pokemon?.name }}</main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { pkmnApi } from '@/api'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import type { Pokemon } from 'pokenode-ts'
+
+const route = useRoute()
+const pokemon = ref<Pokemon>()
+
+onMounted(() => fetchPokemonDetails())
+
+async function fetchPokemonDetails() {
+  if (route.params.pokemonId)
+    pokemon.value = await pkmnApi.getPokemonById(Number(route.params.pokemonId))
+}
+</script>
