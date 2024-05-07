4b0a6fff7f4ad60eee3ea3a964bdd6ddc13d9c01
Alexander Jansen <a.jansen@reanmo.com>
2024-05-07 22:54:54 +0200

    Add german name

--- src/views/PokemonDetailView.vue
+++ src/views/PokemonDetailView.vue
@@ -12,14 +12,16 @@
       clickFunction: () => {}
     }"
   />
-  <main class="bg-grass bg-opacity-80 px-default">{{ pokemon?.name }}</main>
+  <main class="bg-grass bg-opacity-80 px-default text-white">
+    <h1 class="text-4xl font-bold">{{ germanName }}</h1>
+  </main>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { pkmnApi } from '@/api'
 import router from '@/router'
-import type { Pokemon } from 'pokenode-ts'
+import type { PokemonSpecies } from 'pokenode-ts'
 import AppBar from '@/components/AppBar.vue'
 import ArrowIcon from '@/components/icons/ArrowIcon.vue'
 import BurgerIcon from '@/components/icons/BurgerIcon.vue'
@@ -31,11 +33,15 @@ const props = withDefaults(
   {}
 )
 
-const pokemon = ref<Pokemon>()
-
 onMounted(() => fetchPokemonDetails())
 
+const pokemon = ref<PokemonSpecies>()
+
+const germanName = computed(
+  () => pokemon.value?.names.find((name) => name.language.name === 'de')?.name
+)
+
 async function fetchPokemonDetails() {
-  pokemon.value = await pkmnApi.getPokemonByName(props.pokemonName)
+  pokemon.value = await pkmnApi.getPokemonSpeciesByName(props.pokemonName)
 }
 </script>
